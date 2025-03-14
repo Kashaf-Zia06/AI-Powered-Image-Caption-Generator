96fb5684f426e7417faf5e15fdedef04299b3fba
Kashaf Zia <kashafzia100@gmail.com>
2025-03-14 23:23:23 +0500

    Image upload Functionality added

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-var animation = lottie.loadAnimation({
-    container: document.getElementById("ai-animation"), // Target div
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: "https://assets5.lottiefiles.com/packages/lf20_Animation-1741732869439.json" // Replace with the animation URL
+let plus_icon=document.querySelector("#add")
+let btn=document.querySelector("#img-btn")
+let p_img=document.querySelector("#preview-image")
+let img_input=document.querySelector("#file-input")
+let caption_box=document.querySelector("#caption-box")
+let caption=document.querySelector("#caption")
+let drop_box=document.querySelector("#drop-area")
+let cross=document.querySelector("#cross")
+
+plus_icon.addEventListener("click",()=>{
+    img_input.click()
+})
+
+btn.addEventListener("click",()=>{
+    img_input.click()
+})
+img_input.addEventListener('change', (event) => {
+    const file = event.target.files[0];  // Get selected file
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+           p_img.src = e.target.result;  // Display image
+          p_img.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+        plus_icon.style.display='none';
+        drop_box.style.opacity=1;
+        cross.classList.remove("hidden");
+    }
 });
 
+cross.addEventListener("click",()=>{
+    p_img.src = null  // Display image
+    p_img.style.display='none';
+    plus_icon.style.display='block';
+    cross.classList.add("hidden")
+    drop_box.style.opacity=0.6;
+})
 
